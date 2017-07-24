5573e3f19276421da7103e0b6625c5ca28d2a8c0
Feng Liu <liufeng0103@163.com>
2017-07-24 22:15:51 +0800

    添加当bonusList参数为all时查询所有bonus类型的物品

--- src/main/java/com/bnade/wow/controller/AuctionController.java
+++ src/main/java/com/bnade/wow/controller/AuctionController.java
@@ -28,9 +28,15 @@ public class AuctionController {
      */
     @GetMapping
     public List<Auction> findAll(@Valid Auction auction) {
-        // BonusList默认为空
+        /**
+         * auction的bonusList为null时，查询时bonusList将不作为查询条件，这样会查询所有bonusList类型的物品
+         * 而""是一种bonusList类型，get url无法传值""给bonusList
+         * 所以这里约定，如果为null则认为是""， 当指定all是则是查询所有bonusList类型，所以为null
+         */
         if (null == auction.getBonusList()) {
             auction.setBonusList("");
+        } else if ("all".equals(auction.getBonusList())) {
+            auction.setBonusList(null);
         }
         return auctionService.findAll(auction);
     }
