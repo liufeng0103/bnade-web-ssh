e67b7363fdde99fc927fe9ecf7f615b275055bb0
Feng Liu <liufeng0103@163.com>
2017-08-03 13:52:03 +0800

    没有指定卖家和bonusList时默认bonusList为""

--- src/main/java/com/bnade/wow/controller/AuctionController.java
+++ src/main/java/com/bnade/wow/controller/AuctionController.java
@@ -32,8 +32,9 @@ public class AuctionController {
          * auction的bonusList为null时，查询时bonusList将不作为查询条件，这样会查询所有bonusList类型的物品
          * 而""是一种bonusList类型，get url无法传值""给bonusList
          * 所以这里约定，如果为null则认为是""， 当指定all是则是查询所有bonusList类型，所以为null
+         * 当条件指定owner时，我们希望查询这个卖家所有bonusList类型的物品，所以不指定owner和bonusList时才设为""
          */
-        if (null == auction.getBonusList()) {
+        if (null == auction.getOwner() && null == auction.getBonusList()) {
             auction.setBonusList("");
         } else if ("all".equals(auction.getBonusList())) {
             auction.setBonusList(null);
