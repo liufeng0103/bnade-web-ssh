cda2c88888907fab7b1aae50d911da2370e850ee
Feng Liu <liufeng0103@163.com>
2017-08-04 13:51:36 +0800

    pet添加Serializable接口

--- src/main/java/com/bnade/wow/entity/Pet.java
+++ src/main/java/com/bnade/wow/entity/Pet.java
@@ -2,13 +2,14 @@ package com.bnade.wow.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.io.Serializable;
 
 /**
  * 宠物信息
  * Created by liufeng0103@163.com on 2017/6/27.
  */
 @Entity
-public class Pet {
+public class Pet implements Serializable {
     @Id
     private Integer id;
     private String name;
