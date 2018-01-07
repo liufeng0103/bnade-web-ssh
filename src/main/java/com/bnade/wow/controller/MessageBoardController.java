98afe4b85434a40efe9485d8d16bea7b2e51b21f
Feng Liu <liufeng0103@163.com>
2018-01-07 16:41:38 +0800

    去掉不需要的import

--- src/main/java/com/bnade/wow/controller/MessageBoardController.java
+++ src/main/java/com/bnade/wow/controller/MessageBoardController.java
@@ -1,8 +1,6 @@
 package com.bnade.wow.controller;
 
 import com.bnade.wow.entity.MessageBoard;
-import com.bnade.wow.entity.WowToken;
-import com.bnade.wow.repository.WowTokenRepository;
 import com.bnade.wow.service.MessageBoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.List;
 
 /**
  * 留言板
